33d0b88a59fe3b93dd690e325116bf0cbe300d31
Mahendra <mkhati47@gmail.com>
2022-02-20 23:45:51 +0530

    configured laravel websockets and echo

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -38,6 +38,10 @@ return [
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
                 'useTLS' => true,
+                'encrypted' => true,
+                'host' => '127.0.0.1',
+                'port' => 6001,
+                'scheme' => 'http'
             ],
             'client_options' => [
                 // Guzzle client options: https://docs.guzzlephp.org/en/stable/request-options.html
